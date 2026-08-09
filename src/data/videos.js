24d2fded7f5c3ae60fbbf79da2bd64cf662f3de5
lorpinhaa-hub <lorpinhaa@gmail.com>
2026-08-09 20:41:35 -0300

    Create videos.js

---
+++ src/data/videos.js
@@ -0,0 +1,5 @@
+export const VIDEOS = [
+  { id: "1", titulo: "Hino do Guarani", url: "https://www.youtube.com/embed/dQw4w9WgXcQ", categoria: "Clube" },
+  { id: "2", titulo: "Gols do último jogo", url: "https://www.youtube.com/embed/dQw4w9WgXcQ", categoria: "Jogos" },
+  { id: "3", titulo: "Torcida cantando", url: "https://www.youtube.com/embed/dQw4w9WgXcQ", categoria: "Torcida" }
+];
