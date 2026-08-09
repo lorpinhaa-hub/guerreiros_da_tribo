47dc76ad5d07dce67384be6434c171fcd3042fca
lorpinhaa-hub <lorpinhaa@gmail.com>
2026-08-09 20:43:14 -0300

    Create patrocinadores.js

---
+++ src/data/patrocinadores.js
@@ -0,0 +1,8 @@
+export const PATROCINADORES = [
+  { id: "1", nome: "Padaria Tribo", nivel: "ouro", site: "#" },
+  { id: "2", nome: "Restaurante Bugre", nivel: "ouro", site: "#" },
+  { id: "3", nome: "Auto Center Campinas", nivel: "prata", site: "#" },
+  { id: "4", nome: "Mercado do Sócio", nivel: "prata", site: "#" },
+  { id: "5", nome: "Pet Shop Verde", nivel: "bronze", site: "#" },
+  { id: "6", nome: "Pizzaria da Vila", nivel: "bronze", site: "#" }
+];
