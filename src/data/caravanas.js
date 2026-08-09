868f3957de7f826740f4d0690bc578474b8d969e
lorpinhaa-hub <lorpinhaa@gmail.com>
2026-08-09 20:15:19 -0300

    Add CARAVANAS data structure to caravanas.js

--- src/data/caravanas.js
+++ src/data/caravanas.js
@@ -1,1 +1,4 @@
-
+export const CARAVANAS = [
+  { id: "1", preco: 45, data: "22/08/2026", motorista: "Seu Zé", telefone: "(19) 99999-0001", horarioSaida: "13:00", localSaida: "Portão do Brinco de Ouro", destino: "Botafogo-SP (Rio Claro)", vagasTotal: 50, vagasOcupadas: 32 },
+  { id: "2", preco: 60, data: "05/09/2026", motorista: "Tonho", telefone: "(19) 99999-0002", horarioSaida: "12:00", localSaida: "Portão do Brinco de Ouro", destino: "Corinthians (São Paulo)", vagasTotal: 50, vagasOcupadas: 12 }
+];
