f309143563ec5d63d8172a001dee3ab8f964353d
lorpinhaa-hub <lorpinhaa@gmail.com>
2026-08-09 19:50:26 -0300

    Create jogos.js

---
+++ src/data/jogos.js
@@ -0,0 +1,5 @@
+export const JOGOS = [
+  { id: "1", adversario: "Ponte Preta", mandante: true, data: "15/08/2026 16:00", local: "Estádio Brinco de Ouro", campeonato: "Campeonato Paulista" },
+  { id: "2", adversario: "Botafogo-SP", mandante: false, data: "22/08/2026 18:30", local: "Estádio Santa Cruz", campeonato: "Campeonato Paulista" },
+  { id: "3", adversario: "Corinthians", mandante: true, data: "05/09/2026 16:00", local: "Estádio Brinco de Ouro", campeonato: "Copa do Brasil" }
+];
