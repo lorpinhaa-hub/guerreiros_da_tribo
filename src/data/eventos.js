ab6ebaede8b71b74fd4dc27ed185dbb07cccecc3
lorpinhaa-hub <lorpinhaa@gmail.com>
2026-08-09 19:53:10 -0300

    Create eventos.js

---
+++ src/data/eventos.js
@@ -0,0 +1,4 @@
+export const EVENTOS = [
+  { id: "1", nome: "Jogo Guarani x Ponte Preta", tipo: "Jogo", data: "15/08/2026", local: "Estádio Brinco de Ouro" },
+  { id: "2", nome: "Encontro da Torcida", tipo: "Confronto", data: "20/08/2026", local: "Bar do Guarani" }
+];
